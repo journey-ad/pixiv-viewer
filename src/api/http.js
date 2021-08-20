707d80c51d5d15effef9c016421f99040beea837
XCwosjw <53867435+XCwosjw@users.noreply.github.com>
2021-08-20 18:22:32 +0800

    Update http.js
    修复API

--- src/api/http.js
+++ src/api/http.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = 'https://hibiapi.getloli.com/'
+const baseURL = 'https://api.obfs.dev/api'
 
 axios.defaults.baseURL = baseURL
 axios.defaults.timeout = 10000
