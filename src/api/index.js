bfabfaa557f5a7c908323900e76d812d65c8e756
journey-ad <journey.adc@gmail.com>
2024-10-17 19:16:26 +0800

    to: 修改pximg地址

--- src/api/index.js
+++ src/api/index.js
@@ -16,7 +16,7 @@ const isSupportWebP = (() => {
 })();
 
 const imgProxy = url => {
-  let result = url.replace(/i.pximg.net/g, 'pximg.pixiv-viewer.workers.dev')
+  let result = url.replace(/i.pximg.net/g, 'pximg.journey-ad.workers.dev')
 
   if (!isSupportWebP) {
     result = result.replace(/_10_webp/g, '_70')
